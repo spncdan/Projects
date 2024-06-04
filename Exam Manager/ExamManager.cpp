1262a185942d487aa2673770ffa0ca6b2c23854c
dan <121939052+spncdan@users.noreply.github.com>
2024-06-04 09:35:08 -0400

    Update ExamManager.cpp

--- Exam Manager/ExamManager.cpp
+++ Exam Manager/ExamManager.cpp
@@ -1,4 +1,5 @@
 // This program aims to read in the contents of a set of data into three arrays which represents scores on three seperate exams.
+// Language: C++
 // The program will neatly print into an output file the original data and then compute a weighted average of the scores and print that average.
 // Then the program will find the smallest score on each exam and it's position within the array.
 
